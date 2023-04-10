9460f0db517b31573af13f10afecd4e5129fa6f9
Samuel Awud <samuelawud@icloud.com>
2023-04-10 19:34:27 -0400

    header added

--- src/containers/header/header.js
+++ src/containers/header/header.js
@@ -1,8 +1,31 @@
 import React from 'react'
 import './header.css'
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
+
+
 const Header = () => {
   return (
-    <div>header</div>
+      <div className="header section_padding" id="home">
+          <div className="header_content">
+            <h1 className="gradient_text">Let&apos;s Build Something amazing with GPT-4 OpenAI</h1>
+            <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+
+            <div className="header_content_input">
+              <input type="email" placeholder="Your Email Address" />
+              <button type="button">Get Started</button>
+            </div>
+
+            <div className="header_content_people">
+              <img src={people} />
+              <p>1,600 people requested access a visit in last 24 hours</p>
+            </div>
+          </div>
+
+          <div className="header_image">
+            <img src={ai} />
+          </div>
+        </div>
   )
 }
 
