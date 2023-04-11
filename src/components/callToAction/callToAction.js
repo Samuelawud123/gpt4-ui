38acafa87f5bd8d3e0fd75522e1a37ad7965c852
Samuel Awud <samuelawud@icloud.com>
2023-04-11 17:34:21 -0400

    cta

--- src/components/callToAction/callToAction.js
+++ src/components/callToAction/callToAction.js
@@ -2,7 +2,15 @@ import React from 'react'
 import './callToAction.css'
 const CallToAction = () => {
   return (
-    <div>callToAction</div>
+    <div className='cta'>
+      <div className='cta_content'> 
+        <p>Request Early access to get started</p>
+        <h3>Register Today & Start exploring the endless possibilities</h3>
+      </div>
+      <div className='cta_button'>
+        <button type='button'>Get Started</button>
+      </div>
+    </div>
   )
 }
 
