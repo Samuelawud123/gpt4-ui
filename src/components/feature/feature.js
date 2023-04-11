b9c4f0e06be5a0ee15623582bd26604aee40b495
Samuel Awud <samuelawud@icloud.com>
2023-04-11 13:10:25 -0400

    Features and brand components

--- src/components/feature/feature.js
+++ src/components/feature/feature.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import './feature.css'
-const Feature = () => {
+
+const Feature = ({title, text}) => {
   return (
-    <div>feature</div>
+    <div className='features_container_feature'>
+      <div className='features_container_feature_title'>
+          <div/>
+          <h1>{title}</h1>
+      </div>
+      <div className='features_container_feature_text'>
+       <p> {text} </p>
+      </div>
+
+    </div>
   )
 }
 
--- src/containers/whatGPT4/whatGPT4.js
+++ src/containers/whatGPT4/whatGPT4.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import './whatGPT4.css'
+import Feature from '../../components/feature/feature'
 const WhatGPT4 = () => {
   return (
-    <div>whatGPT4</div>
+    <div className='whatgpt4 section-margin' id="wgpt4">
+      <div className='whatgpt4_feature'>
+      <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
+      </div>
+      <div className='whatgpt4_heading'>
+        <h1 className='gradient_text'>
+        The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className='whatgpt4_container'>
+      <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought." />
+      <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+      <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+      </div>
+
+
+    </div>
   )
 }
 
